9e37898a01b07123dd31cdf7ddb40281a4c9b19b
Sahal Ansari <github@sahal.info>
2014-11-13 22:59:19 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,33 @@
+zypper-wrapper
+--------------
+
+This script is a very simple zypper wrapper (but I'm not a Rapper).
+If you run:
+
+# ./zypper-wrapper.sh in $2
+
+and it returns output with "No provider of '$2' found." this script will
+automagically run:
+
+# zypper se $2
+
+NOTES
+-----
+
+The script will (hopefully) fail gracefully if you specify more than
+one pacakge/file name. (Sorry, don't have time to implement that yet.)
+
+If you like the script and want to 'replace' zypper with it (and you
+run bash), just edit your .bashrc (or similar) file to include the
+following line:
+
+alias zypper='/path/to/zypper/wrapper.sh'
+
+Also, I don't have OpenSUSE installed (prefer Debian/Arch) so I'm not
+even sure if this works. If you use this and it doesn't work, please
+file some bug reports on Github, thanks!
+
+LICENSE
+-------
+
+MIT (see LICENSE for details)
---
+++ zypper-wrapper.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# ./zypper-wrapper.sh
+# a very simple zypper wrapper
+# tests if `zypper in $1` failed to find $1 package if it failed it runs `zypper se $1`
+# Copyright (c) 2014 Sahal Ansari - github@sahal.info
+# License: MIT (see LICENSE)
+
+set -e
+
+# the scripts directory
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+# REQUIRED: directory to store tmp files
+TEMP_DIR="$DIR/logs/"
+# NOTE: if you use /tmp/ all users can read packages you (attempt to) install
+#TEMP_DIR="/tmp/"
+
+function zypper { # used for testing on non-OpenSUSE systems
+#  echo "actually zypper ""$@"
+  zypper "$@"
+}
+
+# test if "$1" == "in" - if it is continue; otherwise pass everything to zypper
+# NOTE: not dealing with installing multple packages
+if [[ "$1" != "in" || "$#" != "2" ]]; then
+  zypper "$@"
+  exit
+fi
+
+# lets keep a log of zypper's output
+if [ ! -z "$TEMP_DIR" ]; then
+  mkdir -p "$TEMP_DIR"
+  temp=$(mktemp -p "$TEMP_DIR")
+else
+  echo "\$TEMPDIR is not set, please fix and try again."
+  exit 1
+fi
+
+zypper "$@" | tee "$temp"
+# if zypper does not end with 0
+if [ " ${PIPESTATUS[0]}" -ne "0" ]; then
+  # check output for "'$1' not found in package names."
+#  grep "'$2' not found in package names." < "$temp" &> /dev/null
+  grep "No provider of '$2' found." < "$temp" &> /dev/null
+  if [ "$?" -eq "0" ]; then
+    # have zypper search for "$2" in packages
+    zypper se "$2"
+  fi
+fi
